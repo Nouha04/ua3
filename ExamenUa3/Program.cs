2f286cedd464f6afcb6e35b11cfa1cf995b1096e
Nouha04 <nouha.mouahhidi15@gmail.com>
2023-04-17 12:26:41 -0400

    Les modification de la deuxeme personne

--- ExamenUa3/Program.cs
+++ ExamenUa3/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string nom, prenom, carteCredit, courriel;
+            int numChambre;
+            Client client1;
+            Hotel hotel1;
+            Console.WriteLine("Merci, d'ajouter les informations d'un client ");
+            Console.WriteLine("Le nom: ");
+            nom = Console.ReadLine();
+            Console.WriteLine("Le prenom: ");
+            prenom= Console.ReadLine();
+            Console.WriteLine("Le courriel: ");
+            courriel = Console.ReadLine();
+            Console.WriteLine("Le carteCredit: ");
+            carteCredit= Console.ReadLine();
+            Console.WriteLine("Le numChambre: ");
+            numChambre = Console.ReadLine();
+
+            client1 = new Client(nom, prenom, courriel, carteCredit, numChambre);
+            hotel1 = new Hotel();
+
+            hotel1.AjouterClient(client1);
+
+
         }
     }
 }
