4f6bdea9869e938da7840db337be2388ce10eaec
Nouha04 <nouha.mouahhidi15@gmail.com>
2023-04-17 04:16:20 -0400

    Ajout de la classe hotel

---
+++ ExamenUa3/Hotel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenUa3
+{
+    internal class Hotel
+    {
+        public ArrayList ListeClients { get; set; }
+
+        public Hotel()
+        {
+            ListeClients = new ArrayList();
+        }
+
+        public void AjouterClient(Client client)
+        {
+            ListeClients.Add(client);
+        }
+    }
+}
