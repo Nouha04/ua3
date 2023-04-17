f125da60083ca0959c0c7985f3e219794237a0ad
Nouha04 <nouha.mouahhidi15@gmail.com>
2023-04-17 12:40:19 -0400

    ajout de mon code sans savoir qu'une autre personne a affectué des modifications dans le main

--- ExamenUa3/Program.cs
+++ ExamenUa3/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string nom, prenom, carteCredit, courriel;
+            int numChambre;
+            Client personne1;
+            Hotel hotel;
+            Console.WriteLine("Merci, d'ajouter les informations d'un client ");
+            Console.WriteLine("Le nom: ");
+            nom = Console.ReadLine();
+            Console.WriteLine("Le prenom: ");
+            prenom = Console.ReadLine();
+            Console.WriteLine("Le courriel: ");
+            courriel = Console.ReadLine();
+            Console.WriteLine("Le carteCredit: ");
+            carteCredit = Console.ReadLine();
+            Console.WriteLine("Le numChambre: ");
+            numChambre = int.Parse(Console.ReadLine());
+
+            personne1 = new Client(nom, prenom, courriel, carteCredit, 3);
+            hotel= new Hotel();
+
+            hotel.AjouterClient(personne1);
         }
     }
 }
